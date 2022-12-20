f6e8a21bf1b99b6592ce1cd21a48ef8765a141c3
Antoshina-Shishkina <alenaantoshina>
2022-12-20 11:07:24 +0700

    Кнопка Имя-Фамилия

--- lab9.js
+++ lab9.js
@@ -1,6 +1,18 @@
 function clickMe(button) {
     if(button.innerHTML =='Щелчок')
-    button.innerHTML = 'Спасибо';
+       button.innerHTML = 'Спасибо';
 else
    button.innerHTML = 'Щелчок';
+}
+function clickName(button){
+    if(button.innerHTML =='Ульяна')
+       button.innerHTML = 'Шишкина';
+    else
+       button.innerHTML = 'Ульяна';
+}
+function clickName2(button){
+    if(button.innerHTML =='Алёна')
+       button.innerHTML = 'Антошина';
+    else 
+       button.innerHTML = 'Алёна';
 }
